348c58d15ae2bebd31bed24fbb73184f63f1fdaa
ichang14 <cha24056@byui.edu>
2026-03-07 09:37:00 -0300

    create Resume class

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -10,8 +10,15 @@ class Program
         job1._company = "Microsoft";
         job1._startYear = 2019;
         job1._endYear = 2022;
-
         job1.Display();
+
+        Job job2 = new Job();
+
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        job2.Display();
 
     }
 }
