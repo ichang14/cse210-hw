9d52250f291d1bac481a26a2515f10be99ac45c7
ichang14 <cha24056@byui.edu>
2026-04-16 20:28:53 -0400

    last week

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,30 @@
+public abstract class Activity
+{
+    private string _date;
+    private int _minutes;
+
+    public Activity (string date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public string GetDate()
+    {
+        return _date;
+    }
+
+    public int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public string GetSummary()
+    {
+        return $"{_date} ({_minutes} min) - Distance {GetDistance():F2} kph, Speed {GetSpeed():F2} kph, Pace: {GetPace():F2} min per km";
+    }
+}
